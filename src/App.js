702e3faa8ad466a92ce52bd67e10cf48dd6eefad
refka haddaji <refkahaddaji111@gmail.com>
2022-02-27 21:01:50 +0100

    route

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import './App.css';
+import {
+  Routes,
+  Route,
+} from "react-router-dom";
+import Home from './pages/Home';
+import Contact from './pages/Contact';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <Routes>
+     
+        <Route path="/" element={<Home />} />
+        <Route path="contact" element={<Contact />} />
+   
+    </Routes>
 
-       welcome FROM DONIAAAA
-
-      </header>
-    </div>
   );
 }
 
