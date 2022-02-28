25614b031da6b66dc9763a3e5348c930d2fff92d
Sondes Ahmed <sondes.ahmed.77@gmail.com>
2022-02-28 10:47:54 +0100

    commited

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
-import './App.css';
-import {
-  Routes,
-  Route,
-} from "react-router-dom";
-import Home from './pages/Home';
-import Contact from './pages/Contact';
-import NosFonds from './pages/NosFonds';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
+import NosFonds from "./pages/NosFonds";
+import ResponsabilitéSocial from "./pages/ResponsabilitéSocial";
 
 function App() {
   return (
     <Routes>
-     
-        <Route path="/" element={<Home />} />
-        <Route path="contact" element={<Contact />} />
-        <Route path="nosfonds" element={<NosFonds />} />
-   
+      <Route path="/" element={<Home />} />
+      <Route path="contact" element={<Contact />} />
+      <Route path="nosfonds" element={<NosFonds />} />
+      <Route path="respSocial" element={<ResponsabilitéSocial />} />
     </Routes>
-
   );
 }
 
