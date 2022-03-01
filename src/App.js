020d5e120d9d660c46f5d8b78d92226b781aa4c6
marmotto48 <donia.bromdhane@gmail.com>
2022-03-01 09:00:33 +0100

    design modif 1

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
-import Contact from "./pages/Contact";
-import NosFonds from "./pages/NosFonds";
-import Presentation from "./components/Presentation";
-import News from "./pages/News";
+import Contact from "./pages/Contact/Contact";
+import NosFonds from "./pages/Nos fonds/NosFonds";
+import News from "./pages/News/News";
 import HomeNews from "./components/HomeNews";
 import Homepage from "./pages/Homepage/Homepage";
+import Test from "./pages/Test";
+import Navbar from "./components/navabar/navbar";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Homepage />} />
-      <Route path="contact" element={<Contact />} />
-      <Route path="nosfonds" element={<NosFonds />} />
-      <Route path="presentation" element={<Presentation />} />
-      <Route path="news" element={<News />} />
-      <Route path="homeNews" element={<HomeNews />} />
-    </Routes>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="contact" element={<Contact />} />
+        <Route path="nosfonds" element={<NosFonds />} />
+        <Route path="news" element={<News />} />
+        <Route path="homeNews" element={<HomeNews />} />
+        <Route path="test" element={<Test />} />
+      </Routes>
+      <Footer />
+    </>
   );
 }
 
