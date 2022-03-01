2b933f7c31308e9205a22f56141e8df97969d802
marmotto48 <donia.bromdhane@gmail.com>
2022-03-01 02:00:15 +0100

    Donia 2

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
 import Contact from "./pages/Contact";
 import NosFonds from "./pages/NosFonds";
 import Presentation from "./components/Presentation";
 import News from "./pages/News";
 import HomeNews from "./components/HomeNews";
+import Homepage from "./pages/Homepage/Homepage";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Homepage />} />
       <Route path="contact" element={<Contact />} />
       <Route path="nosfonds" element={<NosFonds />} />
       <Route path="presentation" element={<Presentation />} />
